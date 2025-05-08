1af98a652246a5bda2711a93a408284e896d1f02
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-07 22:22:47 -0700

    Add IProductRepository interface

---
+++ src/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(int id);
+        Task<Product> AddProductAsync(Product entity);
+        Task<Product> UpdateProductAsync(Guid productId, Product entity);
+        Task<bool> DeleteProductAsync(Guid productId);
+    }
+}
