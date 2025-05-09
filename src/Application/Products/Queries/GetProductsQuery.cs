3c9093a7a26b647507a2acfc4e16b12d20d61c8b
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-08 22:53:13 -0700

    Add GetProducts query and handler

---
+++ src/Application/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Products.Queries
+{
+    public record GetProductsQuery() : IRequest<IEnumerable<Product>>;
+    internal class GetProductsQueryHandler(IProductRepository  productRepository)
+        : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        public async Task<IEnumerable<Product>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await productRepository.GetProductsAsync();
+        }
+    }
+}
