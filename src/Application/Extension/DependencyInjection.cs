d2e65a439ee5a2fb273d49e488315f704189570b
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-08 21:10:55 -0700

    Add Application DI and Mediatr services

---
+++ src/Application/Extension/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extension
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services) 
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            return services;
+        }
+    }
+}
--- src/Web/Extension/DependencyInjection.cs
+++ src/Web/Extension/DependencyInjection.cs
@@ -1,11 +1,13 @@
-﻿using Infrastructure.Extension;
+﻿using Application.Extension;
+using Infrastructure.Extension;
 
 namespace Web.Extension
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddAppDI(this IServiceCollection services, IConfiguration configuration) 
-        { 
+        {
+            services.AddApplicationDI();
             services.AddInfrastructureDI(configuration);
 
             return services;
