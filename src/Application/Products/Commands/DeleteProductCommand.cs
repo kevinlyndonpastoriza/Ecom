dddbb856fe67a61a8ee5b8897093261450e1313a
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-08 23:25:03 -0700

    Add DeleteProduct command and handler

---
+++ src/Application/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Products.Commands
+{
+    public record DeleteProductCommand(Guid ProductID) : IRequest<bool>;
+    internal class DeleteProductCommandHandler(IProductRepository productRepository)
+        : IRequestHandler<DeleteProductCommand, bool>
+    {
+        public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            return await productRepository.DeleteProductAsync(request.ProductID);
+        }
+    }
+}
