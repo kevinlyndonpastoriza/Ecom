7aad4b7654043c70f8e79b19f4c745adfd423e38
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-08 23:20:49 -0700

    Add UpdateProduct command and handler

---
+++ src/Application/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Products.Commands
+{
+    public record UpdateProductCommand(Guid ProductID, Product Product) : IRequest<Product>;
+    internal class UpdateProductCommandHandler(IProductRepository productRepository)
+        : IRequestHandler<UpdateProductCommand, Product>
+    {
+        public async Task<Product> Handle(UpdateProductCommand request,  CancellationToken cancellationToken)
+        {
+            return await productRepository.UpdateProductAsync(request.ProductID, request.Product);
+        }
+    }
+}
