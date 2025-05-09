9146f2d3a9a1a1d9d96dc15d4559493a4c015cb6
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-09 01:49:37 -0700

    add MapEndpoints extension for automatic endpoint discovery

---
+++ src/Web/Infrastructure/WebApplicationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Web.Infrastructure
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication MapEndpoints(this WebApplication app) 
+        {
+            var endpointGroupType = typeof(EndpointGroupBase);
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var endpointGroupTypes = assembly.GetExportedTypes()
+                .Where(t => t.IsSubclassOf(endpointGroupType));
+
+            foreach (var type in endpointGroupTypes)
+            {
+                if (Activator.CreateInstance(type) is EndpointGroupBase instance)
+                {
+                    instance.Map(app);
+                }
+            }
+
+            return app;
+        }
+    }
+}
