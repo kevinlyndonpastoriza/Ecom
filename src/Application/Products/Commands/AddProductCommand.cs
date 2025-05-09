cbbea8f790fee68a3ef89a223071fcd99c73e48a
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-08 23:12:56 -0700

    Add AddProduct command and handler

---
+++ src/Application/Products/Commands/AddProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Products.Commands
+{
+    public record AddProductCommand(Product Product) : IRequest<Product>;
+    internal class AddProductCommandHandler(IProductRepository productRepository)
+        : IRequestHandler<AddProductCommand, Product>
+    {
+        public async Task<Product> Handle(AddProductCommand request,  CancellationToken cancellationToken)
+        {
+            return await productRepository.AddProductAsync(request.Product);
+        }
+    }
+}
