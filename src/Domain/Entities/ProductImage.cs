4ef7265132e481cea972bfb62c6c2f259da9e960
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-13 05:25:17 -0700

    Add ProductImage entity and to AppDbContext

---
+++ src/Domain/Entities/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ProductImage
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; } = default!;
+        public string? Alt { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; } = default!;
+    }
+}
--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -11,5 +11,6 @@ namespace Infrastructure.Data
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
     }
 }
