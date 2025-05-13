d2345ed35136b2eee637adc5c7dca09de9476c2a
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-13 06:05:58 -0700

    Add Brand entity and to AppDbContext

--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -13,5 +13,6 @@ namespace Infrastructure.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Brand> Brands { get; set; }
     }
 }
