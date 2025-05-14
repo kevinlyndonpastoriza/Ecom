e1273d1d619107f906f14d63ec4e754eab42a45a
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-14 04:54:26 -0700

    Add MessageModel Dto

---
+++ src/framework/Types/Dto/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types.Dto
+{
+    [Serializable]
+    public class MessageModel
+    {
+        public MessageModel(string id, string detail, SeverityType severity) 
+        { 
+            Id = id;
+            Detail = detail;
+            Severity = severity;
+        }
+
+        public string Id { get; }
+        public string Detail { get; }
+        public SeverityType Severity { get; }
+    }
+}
