6d8918c8e30864933bd6af7acb5ebd8f0bb43806
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-14 05:42:39 -0700

    Add s ValidationErrorMod Dto

---
+++ src/framework/Types/Dto/ValidationErrorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types.Dto
+{
+    public class ValidationErrorModel
+    {
+        public ValidationErrorModel(string field, IEnumerable<string> validationErrors)
+        {
+            Field = field;
+            ValidationErrors = validationErrors;
+        }
+
+        public string Field { get; }
+        public IEnumerable<string> ValidationErrors { get; }
+    }
+}
