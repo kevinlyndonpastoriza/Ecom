74cb05558479dc7d51c4ba3023c629ab6d135f4f
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-14 04:37:38 -0700

    Add BaseApiResponse Dto

---
+++ src/framework/Types/Dto/BaseApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types.Dto
+{
+    public abstract class BaseApiResponse
+    {
+        public BaseApiResponse(ResponseStatus status)
+        {
+            Status = status;
+        }
+
+        public ResponseStatus Status { get; }
+    }
+}
