335109ff0f71a61adea1d92bf4d9d38f68980b87
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-14 05:47:28 -0700

    Add ModelValidationErrorResponse Dto

---
+++ src/framework/Types/Dto/ModelValidationErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types.Dto
+{
+    public class ModelValidationErrorResponse : BaseApiResponse
+    {
+        /// <summary>
+        ///     Default constructor
+        /// </summary>
+        /// <param name="validation">Validation information to return in response</param>
+        public ModelValidationErrorResponse(IEnumerable<ValidationErrorModel> validation)
+            : base(ResponseStatus.Failed) 
+        {
+            Validation = validation;
+        }
+
+        public IEnumerable<ValidationErrorModel> Validation { get; }
+    }
+}
