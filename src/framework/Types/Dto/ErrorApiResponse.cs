956d6bf3015da9155f787a1aefd13bb83f39e587
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-05-14 05:01:46 -0700

    Add ErrorApiResponse Dto

---
+++ src/framework/Types/Dto/ErrorApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types.Dto
+{
+    public class ErrorApiResponse : BaseApiResponse
+    {
+        /// <summary>
+		///     Default constructor
+		/// </summary>
+		/// <param name="error">Data to return in response</param>
+		/// <param name="messages">Messages to include in response</param>
+		/// <param name="status">Response status (success or failed)</param>
+        public ErrorApiResponse(object error)
+            : base(ResponseStatus.Failed)
+        {
+            Error = error;
+        }
+
+        public object Error { get; }
+    }
+}
